ce74710a1d359d621359c8aef0adf0faa1884aaa
MinervaNL <49182038+MinervaNL@users.noreply.github.com>
2024-08-12 16:15:06 -0700

    create review into mongoDB

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const { campgroundSchema } = require('./schemas.js');
-const catchAsynx = require('./utils/catchAsync');
+const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
-const catchAsync = require('./utils/catchAsync');
+const Review = require('./models/review');
+
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -81,7 +82,12 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
 }))
 
 app.post('/campgrounds/:id/reviews', catchAsync(async (req, res) => {
-    res.send("Yes");
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
 }))
 
 app.all('*', (req, res, next) => {
